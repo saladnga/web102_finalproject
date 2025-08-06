d8ddd75df3cc34701442990efdf040607af4041e
Vu Hoang <vulego2004@gmail.com>
2025-08-07 02:12:00 +0700

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+});
